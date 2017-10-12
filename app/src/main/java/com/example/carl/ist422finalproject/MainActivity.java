393d3b60c228d1da405bc6140331c2b9c4f4f8e9
carl <dibertcarl@gmail.com>
2017-10-12 14:45:58 -0500

    Secondary Test Commit

--- app/src/main/java/com/example/carl/ist422finalproject/MainActivity.java
+++ app/src/main/java/com/example/carl/ist422finalproject/MainActivity.java
@@ -10,10 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void test_method(){
-
-        int i = 1+1;
-
-    }
 }
