2eadf16110c762929e9d08ad113e3d02c0aa68b8
Tyler Brown <tyler0726@gmail.com>
2017-10-12 14:48:40 -0500

    commiutt

--- app/src/main/java/com/example/carl/ist422finalproject/MainActivity.java
+++ app/src/main/java/com/example/carl/ist422finalproject/MainActivity.java
@@ -10,4 +10,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    public void test_method(){
+
+
+
+        whuyyyyy;
+
+
+        String hi = "High";
+    }
+            }
