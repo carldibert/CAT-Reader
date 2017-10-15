f16e17e75c8f8c715d0926c18f1c788f02db7a7b
Carl Dibert <dibertcarl@gmail.com>
2017-10-14 22:38:41 -0500

    Added web crawler that can be changed around for future uses. Ignore naming conventions for sample button and sample text

--- app/src/main/java/com/example/carl/ist422finalproject/MainActivity.java
+++ app/src/main/java/com/example/carl/ist422finalproject/MainActivity.java
@@ -1,30 +1,71 @@
 package com.example.carl.ist422finalproject;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.net.*;
+import java.io.*;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
+
+import static java.lang.annotation.ElementType.METHOD;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button funShitButton = (Button) findViewById(R.id.funShitButton);
+        funShitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                new internet().execute();
+            }
+        });
     }
 
-    String high = "hello";
+    public void changeTextOnTest(){
 
-    String Rick = "Wubba"
-            + "Lubba" +
-            "Dub" +
-            "duuuuuub"
-            ;
+        new internet().execute();
 
-    String cheese = "Damn, Get em next time";
+    }
 
-    String boi = "Ya, you lost it";
+    public class internet extends AsyncTask<Void, Void, Void> {
+
+        String words;
+
+        @Override
+        protected Void doInBackground(Void... params){
+
+            try{
+                Document doc = Jsoup.connect("http://www.mangareader.net/")
+                        .followRedirects(false)
+                        .get();
+
+                words = doc.title();
+            } catch(Exception e){
+                e.printStackTrace();
+            }
 
 
-    String git = "GitHub Is POS and if they are monitoring what i am writing, t(-.-t)";
+            return null;
+        }
 
-    String stringCheeseBoi = "String Cheese bois";
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            TextView text = (TextView) findViewById(R.id.funShitText);
+            text.setText(words);
+        }
+    }
 }
