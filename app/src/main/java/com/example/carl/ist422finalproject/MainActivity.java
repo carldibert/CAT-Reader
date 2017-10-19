0400295c8d34a76c8997760f94ba4d2fca026c8a
carl <dibertcarl@gmail.com>
2017-10-19 15:01:50 -0500

    Added new Activity with test button to switch activities

--- app/src/main/java/com/example/carl/ist422finalproject/MainActivity.java
+++ app/src/main/java/com/example/carl/ist422finalproject/MainActivity.java
@@ -3,20 +3,12 @@ package com.example.carl.ist422finalproject;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.content.Intent;
 
-import java.net.*;
-import java.io.*;
-
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
-import org.jsoup.select.Elements;
-
-import static java.lang.annotation.ElementType.METHOD;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
 //                new internet().execute();
 //            }
 //        });
+
+        Button sendToMainScreen = (Button) findViewById(R.id.sendToSearchResult);
+        sendToMainScreen.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, SearchResult.class));
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/example/carl/ist422finalproject/SearchResult.java
@@ -0,0 +1,21 @@
+package com.example.carl.ist422finalproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by carl on 10/19/17.
+ */
+
+public class SearchResult extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_result_page);
+
+
+
+    }
+
+}
