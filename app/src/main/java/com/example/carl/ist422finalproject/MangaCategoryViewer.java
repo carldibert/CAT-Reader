224d231190122485d9458664f8669abcfc74560d
Tyler Brown <tyler0726@gmail.com>
2017-11-07 01:53:44 -0600

    I honestly have no idea what I'm doing. I tried and found the code but I cant decifer where what variable goes.

--- app/src/main/java/com/example/carl/ist422finalproject/MangaCategoryViewer.java
+++ app/src/main/java/com/example/carl/ist422finalproject/MangaCategoryViewer.java
@@ -1,26 +1,27 @@
 package com.example.carl.ist422finalproject;
 
-import android.app.Activity;
 import android.app.ListActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Carl on 10/26/2017.
  */
 
-public class MangaCategoryViewer extends ListActivity{
+public class MangaCategoryViewer extends ListActivity {
+    ArrayList<HashMap<String, String>> Item_List;
+    ArrayList stock_list;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manga_categories);
 
@@ -35,11 +36,11 @@ public class MangaCategoryViewer extends ListActivity{
         ArrayList<String> sam = new ArrayList<String>();
 
         @Override
-        protected Void doInBackground(Void... params){
+        protected Void doInBackground(Void... params) {
 
-            ArrayList<String> copy  = new ArrayList<String>();
+            ArrayList<String> copy = new ArrayList<String>();
 
-            try{
+            try {
                 Document doc = Jsoup.connect("http://www.mangareader.net/search")
                         .followRedirects(false)
                         .get();
@@ -47,16 +48,16 @@ public class MangaCategoryViewer extends ListActivity{
 
                 Elements genres = doc.getElementsByClass("genre r0");
 
-                for(int i=0; i<genres.size(); i++)
+                for (int i = 0; i < genres.size(); i++)
                     copy.add(genres.get(i).toString());
-            } catch(Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
             String[] n;
             String[] t;
             String temp = "";
-            for(int i=0; i<copy.size(); i++){
+            for (int i = 0; i < copy.size(); i++) {
                 n = copy.get(i).split(">");
                 t = n[1].toString().split("<");
                 temp = t[0].toString().replaceAll("\\s+", "");
@@ -70,9 +71,35 @@ public class MangaCategoryViewer extends ListActivity{
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
 
+
             String[] categoriesArray = sam.toArray(new String[sam.size()]);
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_list_item_1, categoriesArray);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(),
+                    android.R.layout.simple_list_item_1, categoriesArray);
             getListView().setAdapter(adapter);
+
+
+//      I have no Idea What I'm Doing
+//            getListView().setOnClickListener(new AdapterView.OnItemClickListener() {
+//                @Override
+//                public void onItemClick(AdapterView<?> parent, View view, int position,
+//                                        long id) {
+//                    stock_list.add(Item_List.get(position));
+
+
+//                    .setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            Intent modify_intent = new Intent(grabedDb.this,
+//                                    updatedata.class);
+//                            modify_intent.putStringArrayListExtra("stock_list", stock_list);
+//
+//                            startActivity(modify_intent);
+//                        }
+//                    });
+//        }
+//
+//            });
+
         }
     }
 }
